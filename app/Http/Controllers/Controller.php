6e220d111a13646168d9183f60e0f3423a936e84
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2024-06-22 08:47:16 -0700

    add dark banner

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -24,6 +24,7 @@ abstract class Controller
             'district',
             'name',
             'banner',
+            'banner_dark',
             'website',
             'language'
         ];
