87874211972b5619df41ca3607bea4960f25536d
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2024-09-20 21:19:01 -0700

    replace map

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="/favicon.png">
 
-    <title>District Map</title>
+    <title>Area Map</title>
 
     @vite('resources/css/app.css')
 </head>
@@ -14,12 +14,8 @@
 <body>
     <div id="map" class="h-dvh"></div>
 
-    <!-- prettier-ignore -->
-    <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-    ({key: "{{ env('VITE_GOOGLE_API_KEY') }}", v: "weekly"});</script>
-
-    @vite('resources/js/app.js')
-
+    <script src="https://area-map.netlify.app/app.js" type="module" async></script>
+    <link rel="stylesheet" href="https://area-map.netlify.app/app.css" />
 </body>
 
 </html>
