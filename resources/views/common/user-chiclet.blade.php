6062b133901632d11270b0124edd59c2d64507d7
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2024-10-16 08:33:10 -0700

    tweak colors

--- resources/views/common/user-chiclet.blade.php
+++ resources/views/common/user-chiclet.blade.php
@@ -1,4 +1,4 @@
-<span class="border rounded-full flex px-3 py-1 items-center gap-2">
+<span class="border rounded-full flex px-3 py-1 items-center gap-2 border-gray-500">
     {{ $user->email }}
     <a href="{{ route('remove-user', [$user->id, isset($entity) ? $entity->id : null]) }}">
         @include('common.icon', ['icon' => 'x', 'size' => 'size-5'])
--- resources/views/livewire/email-form.blade.php
+++ resources/views/livewire/email-form.blade.php
@@ -1,13 +1,14 @@
 <form class="flex items-center gap-3" method="post" action="{{ route('add-user', compact('entity')) }}">
     @csrf
     @if (!$show)
-        <button wire:click="$set('show', true)" class="bg-transparent border rounded-full px-3 py-1" type="button">
+        <button wire:click="$set('show', true)" class="bg-transparent border rounded-full border-gray-500 px-3 py-1"
+            type="button">
             @include('common.icon', ['icon' => 'plus'])
         </button>
     @else
         <div class="relative">
             <input type="email" name="email" autofocus autocomplete="off" data-1p-ignore
-                class="bg-transparent border rounded-full px-4 py-1">
+                class="bg-transparent border rounded-full px-4 py-1 border-gray-500">
             <button wire:click="$set('show', false)" class="absolute right-3 inset-y-1/2 -mt-2.5" type="button">
                 @include('common.icon', ['icon' => 'x', 'size' => 'size-5'])
             </button>
